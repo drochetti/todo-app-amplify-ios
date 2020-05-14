c38a89c2634e822fed75ab5799c7f7650613dc2b
Daniel Rochetti <daniel.rochetti@gmail.com>
2020-05-14 15:21:34 -0700

    initial commit

---
+++ TodoAmplify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoAmplify
+//
+//  Created by Rochetti, Daniel on 5/14/20.
+//  Copyright © 2020 Amazon Web Services. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
